11805a7c353273767d530fea50e7a1f88e10b1ef
jiseonsim <90298667+jiseonsim@users.noreply.github.com>
2021-09-08 12:23:46 +0900

    프로그램 버전 1.1
    Hello World를 6번 출렬하도록 수정

--- Hello World.cpp
+++ Hello World.cpp
@@ -3,6 +3,11 @@
 int main() {
 	
 	printf("Hello World!");
-	
+	printf("Hello World!");
+	printf("Hello World!");
+	printf("Hello World!");
+	printf("Hello World!");
+	printf("Hello World!");
+						
 	return 0;	
 }
